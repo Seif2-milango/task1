cd0bd16f5358395f1782b48656d79b7460da06ab
Seif Ewais <seif.ahmed@milango.app>
2026-01-11 16:30:24 +0200

    refactor(number input): make the number component reusable instead of re-writing it everytime
    This will make it the code easier to read and edit (good coding practice)

---
+++ components/ui/inputs/horizontalNum.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { StyleSheet, TextInput } from "react-native";
+import { ThemedText } from "../../themed-text";
+import { ThemedView } from "../../themed-view";
+
+type NumberInputProps = {
+  label: string;
+  value: string;
+  setValue: (text: string) => void;
+  placeholder?: string;
+  regex: RegExp;
+  errorMessage: string;
+  maxLength?: number;
+};
+
+export default function NumberInput({
+  label,
+  value,
+  setValue,
+  placeholder = "Enter here",
+  regex,
+  errorMessage,
+  maxLength = 11,
+}: NumberInputProps) {
+  const [error, setError] = useState<string | null>(null);
+
+  const validate = (text: string) => {
+    setValue(text);
+    if (!text) return setError(null);
+    setError(!regex.test(text) ? errorMessage : null);
+  };
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.label}>{label}</ThemedText>
+      <ThemedView style={{ width: "60%" }}>
+        <TextInput
+          value={value}
+          onChangeText={validate}
+          placeholder={placeholder}
+          placeholderTextColor="#9CA3AF"
+          keyboardType="number-pad"
+          maxLength={maxLength}
+          style={[styles.input, error ? styles.inputError : null]}
+        />
+        {error && <ThemedText style={styles.errorText}>{error}</ThemedText>}
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    marginBottom: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 6,
+    fontWeight: "500",
+    paddingHorizontal: 15,
+  },
+  input: {
+    height: 60,
+    borderWidth: 1,
+    borderColor: "#d1d1d1ff",
+    borderRadius: 15,
+    paddingHorizontal: 14,
+    fontSize: 16,
+    backgroundColor: "#FFFFFF",
+    width: "100%",
+    textAlign: "center",
+  },
+  inputError: {
+    borderColor: "#EF4444",
+  },
+  errorText: {
+    marginTop: 6,
+    fontSize: 13,
+    color: "#EF4444",
+  },
+});
